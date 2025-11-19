44d489185b1478b69ad74f2f1d48440a6268b96b
venkatadevops1995 <98838123+venkatadevops1995@users.noreply.github.com>
2025-11-19 14:19:23 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,16 +1,70 @@
+# Provider configuration
 provider "aws" {
-  region = "ap-south-1"
-  access_key = "AKIAUDRNICT5W7OMRENH"
-  secret_key = "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"
+  region = var.aws_region
+  # Remove hardcoded credentials - use AWS credentials file or environment variables instead
 }
 
+# Variables
+variable "aws_region" {
+  description = "AWS region for resources"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "instance_count" {
+  description = "Number of EC2 instances to create"
+  type        = number
+  default     = 2
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+  default     = "ami-0a4408457f9a03be3"
+}
+
+variable "instance_name_prefix" {
+  description = "Prefix for instance names"
+  type        = string
+  default     = "myinstance"
+}
+
+# EC2 Instances
 resource "aws_instance" "instance" {
-  instance_type = "t3.micro"
-  ami = "ami-0a4408457f9a03be3"
-  count = 2
+  count                       = var.instance_count
+  instance_type               = var.instance_type
+  ami                         = var.ami_id
   associate_public_ip_address = true
+
   tags = {
-    Name = "myinstance"
+    Name        = "${var.instance_name_prefix}-${count.index + 1}"
+    Environment = "development"
+    ManagedBy   = "Terraform"
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
+# Outputs
+output "instance_ids" {
+  description = "IDs of created EC2 instances"
+  value       = aws_instance.instance[*].id
+}
+
+output "instance_public_ips" {
+  description = "Public IP addresses of EC2 instances"
+  value       = aws_instance.instance[*].public_ip
+}
+
+output "instance_private_ips" {
+  description = "Private IP addresses of EC2 instances"
+  value       = aws_instance.instance[*].private_ip
+}
