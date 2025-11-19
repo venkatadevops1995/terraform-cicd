7057aa1fffc9da957035ca3db9d0a98be5e48cfc
venkatadevops1995 <98838123+venkatadevops1995@users.noreply.github.com>
2025-11-19 14:29:23 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -53,18 +53,3 @@ resource "aws_instance" "instance1" {
   }
 }
 
-# Outputs
-output "instance_ids" {
-  description = "IDs of created EC2 instances"
-  value       = aws_instance.instance[*].id
-}
-
-output "instance_public_ips" {
-  description = "Public IP addresses of EC2 instances"
-  value       = aws_instance.instance[*].public_ip
-}
-
-output "instance_private_ips" {
-  description = "Private IP addresses of EC2 instances"
-  value       = aws_instance.instance[*].private_ip
-}
